ce51c20a8f60cf0b9cd8fcf44354a5b539d87d75
Catherine <clasesdam0@gmail.com>
2023-11-03 13:58:03 +0100

    3/11

--- Ejercicios3/Ej10.java
+++ Ejercicios3/Ej10.java
@@ -11,8 +11,23 @@ public class Ej10{
         Scanner lect = new Scanner(System.in);
         System.out.println("Escribe un número entero para calcular su factorial:");
         int num = lect.nextInt();
+        long res = calculoFactorial(num);
 
-        System.out.println("El factorial de " + num + " es " + calculoFactorial());
-   
+        System.out.println("El factorial de " + num + " es " + res);
+    }
+
+    static long calculoFactorial(int a){
+        if (a == 0) {
+            System.out.println("1");
+        }
+        if (a < 0) {
+            System.out.println("Los números negativos no tienen factorial");
+        }
+        long fact = 1;
+        for (int i = 1; i <= a; i++) {
+            fact *= i;
+        }
+        return fact;
+
     }
 }
--- Ejercicios3/Ej11.java
+++ Ejercicios3/Ej11.java
@@ -1,2 +1,40 @@
 //Catherine AM
 package Ejercicios3;
+
+import java.util.Scanner;
+
+public class Ej11{
+    public static void main(String[] args) {
+        System.out.println("Ejercicio 11");
+        System.out.println(" ");
+
+        Scanner lect = new Scanner(System.in);
+        System.out.println("Escribe una secuencia de 4 n´umeros para saber si es capicúa");
+        System.out.println("Proporciona un número(1): ");
+        int num1 = lect.nextInt();
+
+        System.out.println("Proporciona un número(2): ");
+        int num2 = lect.nextInt();
+
+        System.out.println("Proporciona un número(3): ");
+        int num3 = lect.nextInt();
+
+        System.out.println("Proporciona un número(4): ");
+        int num4 = lect.nextInt();
+
+        int res = esCapicua(num1,num2,num3,num4);
+
+        if (res == ) {
+            
+        } else {
+            System.out.println("");
+        }
+                
+    }
+
+    static int esCapicua(int a,int b,int c,int d){
+
+        return d;
+
+    }
+}
