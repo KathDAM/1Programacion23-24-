d19dbaee943f8762b937dec00014596baa1c2c31
Catherine <clasesdam0@gmail.com>
2023-11-09 10:44:32 +0100

    subida ultima

--- unidad5/ejercicio3/Punto.java
+++ unidad5/ejercicio3/Punto.java
@@ -5,7 +5,42 @@ public class Punto {
     //ATRIBUTOS
     private double x,y;
 
-    public Punto() {
-        super();
+    //CONSTRUCTOR
+    public Punto(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
+
+    //GETTER
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+
+    //SETTER
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    //METODOS
+    public void imprimirCoordenadas(){
+        System.out.println("Coordenada X: " + x);
+        System.out.println("Coordenada Y: " + y);
+    }
+
+    public double calcularDistanciapuntoapunto(){
+        return 0;
+    }
+
+    public double calcularDistanciaotropunto(){
+        return 0;
+    }
+
+
 }
