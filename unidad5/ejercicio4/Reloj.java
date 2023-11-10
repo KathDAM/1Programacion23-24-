dc35e718d4bc04c86eaa8ae8a38d078f96b13e17
Catherine <clasesdam0@gmail.com>
2023-11-10 13:55:05 +0100

    13.34

--- unidad5/ejercicio4/Reloj.java
+++ unidad5/ejercicio4/Reloj.java
@@ -2,5 +2,19 @@ package unidad5.ejercicio4;
 
 public class Reloj {
     //ATRIBUTOS
-    
+    private int hora,min,seg;
+
+    //CONSTRUCTOR SIN PARAMETROS
+    public Reloj() {
+        this.hora = hora;
+        
+    }
+
+    //CONSTRUCTOR
+    public Reloj() {
+        super();
+    }
+
+
+
 }
