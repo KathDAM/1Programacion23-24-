f87f0b8c7e476c8aceeb7f5dc3af1582e9975241
Kathy <clasedam@gmail.com>
2023-11-15 18:40:57 +0100

    punto fin

--- unidad5/ejercicio3/Punto.java
+++ unidad5/ejercicio3/Punto.java
@@ -5,14 +5,20 @@ public class Punto {
     // ATRIBUTOS
     private double x, y;
 
-    // CONSTRUCTOR
+    // CONSTRUCTOR CON PARAMETROS
     public Punto(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
+    //CONSTRUCTOR SIN PARAMETROS
+    public Punto() {
+        this.x = 0;
+        this.y = 0;
+    }
+
     // GETTER
-    public double getGetX() {
+    public double getX() {
         return x;
     }
 
@@ -21,7 +27,7 @@ public class Punto {
     }
 
     // SETTER
-    public void setGetX(double x) {
+    public void setX(double x) {
         this.x = x;
     }
 
@@ -31,18 +37,37 @@ public class Punto {
 
     // METODOS
     public void imprimirCoordenadas() {
-        System.out.print("(" + x);
-        System.out.print(" , " + y + ")");
+        System.out.print("(" + x + " , " + y + ")");
         System.out.println();
     }
 
-    public double calcularDistanciapuntoapunto(Punto puntoB) {
-        result.x = this.x -puntoB;
-        return x;
+    //calcula distancia a otro punto
+    public double distancia(Punto otroPunto) {
+        double distanciaX = this.x - otroPunto.getX();
+        double distanciaY = this.y - otroPunto.getY();
+        return Math.sqrt(distanciaX * distanciaX + distanciaY * distanciaY);
     }
 
-    public double calcularDistanciaotropunto() {
-        return 0;
+    //calcula distancia al origen
+    public double distancia() {
+        return Math.sqrt(x * x + y * y);
+
     }
+
+//--------------------------------------------------------------------------------------------------------
+
+/* 
+    //calcula la distacia a otro punto
+    public double calcularDistancia(Punto puntoB) {
+        double distanciaX = this.x - puntoB.x; //se puede poner get
+        double distanciaY = this.y - puntoB.y;
+        return Math.sqrt((distanciaX * distanciaX) + (distanciaY * distanciaY));
+    }
+
+    //calcula la distancia al origen
+    public double calcularDistanciaOrigen() {
+        return Math.sqrt(x * x + y * y);
+    }
+*/
 
 }
--- unidad5/ejercicio3/PuntoTest.java
+++ unidad5/ejercicio3/PuntoTest.java
@@ -7,7 +7,8 @@ public class PuntoTest {
         Punto punto1 = new Punto(0, 0);
         Punto punto2 = new Punto(5, 3);
         Punto punto3 = new Punto(2, -1);
-        Punto punto4 = new Punto((punto2.getGetX() + punto3.getGetX()) / 2, (punto2.getY() + punto3.getY()) / 2);
+        Punto punto4 = new Punto((punto2.getX() + punto3.getX()) / 2, (punto2.getY() + punto3.getY()) / 2);
+        Punto punto5 = new Punto(4, 3);
         
         System.out.print("Punto 1: ");
         punto1.imprimirCoordenadas();
@@ -21,5 +22,25 @@ public class PuntoTest {
         System.out.print("Punto 4: ");
         punto4.imprimirCoordenadas();
 
+        //Calcula la distancia de punto 1 a punto 2
+        double distanciaEntrePuntos = punto1.distancia(punto2);
+        System.out.println("Distancia entre puntos: " + distanciaEntrePuntos);
+
+        //Calcula y muestra distancia del punto(4,3) al origen 
+        double distanciaAlOrigen = punto5.distancia();
+        System.out.println("Distancia al origen: " + distanciaAlOrigen);
+
+//--------------------------------------------------------------------------------------------------------
+
+/* 
+        //Calcula la distancia de punto 1 a punto 2
+        double distancia = punto1.calcularDistancia(punto2);
+        System.out.println("La distancia entre los puntos es: " + distancia);
+
+        // Calcula y muestra distancia del punto(4,3) al origen 
+        Punto punto5 = new Punto(4, 3);
+        double distanciaAlOrigen = punto5.calcularDistanciaOrigen();
+        System.out.println("Distancia del punto (4,3) al origen: " + distanciaAlOrigen);
+*/  
     }
 }
