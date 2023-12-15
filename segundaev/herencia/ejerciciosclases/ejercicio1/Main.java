e55c76e1a9afbdf4da0661521b2af34a33eea453
Catherine <clasesdam0@gmail.com>
2023-12-15 13:47:03 +0100

    15/12

--- segundaev/herencia/ejerciciosclases/ejercicio1/Main.java
+++ segundaev/herencia/ejerciciosclases/ejercicio1/Main.java
@@ -3,13 +3,12 @@ package segundaev.herencia.ejerciciosclases.ejercicio1;
 public class Main {
     public static void main(String[] args) {
         Examen examen1 = new Examen();
-        Fecha fecha1 = new Fecha();
+        Fecha fecha1 = new Fecha(1,5,2010);
         
         examen1.nombre = "Matematicas";
         examen1.aula = 2;
-        fecha1.fecha = 12/07/1998;
-       // examen1.hora = 09:30;
 
         examen1.toString(); 
+        fecha1.toString();
     }
 }
---
+++ segundaev/herencia/ejerciciosclases/ejercicio2/Punto.java
@@ -0,0 +1,37 @@
+package segundaev.herencia.ejerciciosclases.ejercicio2;
+
+public class Punto {
+    // ATRIBUTOS
+    protected int x,y;
+
+    // CONSTRUCTOR
+    public Punto(int x, int y) {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    // SET
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    // GET
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    // METODOS
+    public int distancia(){
+        return 0;
+    }
+
+
+}
